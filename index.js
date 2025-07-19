3a877461b24c73125bd8534fc54ece7e1994bd70
BlueNightFury46 <Celestial@shadowtrilogy.co.uk>
2025-07-19 20:10:31 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const discord = require('discord.js')
+//I'm not great at JS, so this might be spaghetti code lol...
+const discord = require('discord.js');
+//Appearantly this is a popular module to not leak your bot token
+const dotenv = require('dotenv');
 
 
+//Events seems to be an enum with a list of possible events to select, the second argument seems to just be a basic lambda
+discord.once(Events.ClientReady, (event_data) =>{
+
+     
+
+});
+
+
+
+
+discord.login(/*TOKEN GOES HERE*/);
